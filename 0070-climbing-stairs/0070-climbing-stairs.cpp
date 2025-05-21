aa18197db5e8fe6c65b07cd01ddca9f29d5dca56
AmanDeep <162536127+imaman2001@users.noreply.github.com>
2025-05-21 23:36:45 +0530

    Time: 0 ms (100%), Space: 8.6 MB (34.22%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,30 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        // Tabulation DP method (bottom - top)
-        vector<int> dp(n+1, 0);
-        dp[0] = 1;
-        dp[1] = 1;
-
-        for(int i=2; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+    int helper(int n, vector<int>& dp){
+        if(n == 0 || n == 1){
+            return 1;
         }
+
+        if(dp[n] != -1){    // if dp[n] is already visited
+            return dp[n];
+        }
+
+        dp[n] = helper(n-1 , dp) + helper(n-2, dp);
         return dp[n];
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1, -1);
+
+        return helper(n, dp);
+
+        // // Tabulation DP method (bottom - top)
+        // vector<int> dp(n+1, 0);
+        // dp[0] = 1;
+        // dp[1] = 1;
+
+        // for(int i=2; i<=n; i++){
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // }
+        // return dp[n];
     }
 };
