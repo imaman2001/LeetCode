6da8d0478f0775c863cda2388e310edcea342d87
AmanDeep <162536127+imaman2001@users.noreply.github.com>
2026-01-10 08:50:35 +0530

    Time: 3 ms (68.17%), Space: 67.7 MB (22.04%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -11,18 +11,38 @@ private:
     }
 public:
     int findDuplicate(vector<int>& nums) {
-        int i = 1; 
-        int j = nums.size() - 1;
+        // int i = 1; 
+        // int j = nums.size() - 1;
 
-        while(i <= j){
-            int mid = i + (j - i) / 2;
-           if(check(nums , mid) > mid){
-            j = mid - 1;
-           }
-           else {
-            i = mid + 1;
-           }
+        // while(i <= j){
+        //     int mid = i + (j - i) / 2;
+        //    if(check(nums , mid) > mid){
+        //     j = mid - 1;
+        //    }
+        //    else {
+        //     i = mid + 1;
+        //    }
+        // }
+        // return i;
+
+    // ......... BF ............ Approach.................
+        // sort(nums.begin() , nums.end());
+        // for(int i=0; i<nums.size(); i++){
+        //     if(nums[i] == nums[i+1]){
+        //         return nums[i];
+        //     }
+        // }
+        // return 0;
+
+    // ....... Better Approach ...................
+        vector<int>freq(nums.size() + 1 , 0);
+        for(int i=0; i<nums.size(); i++){
+            if(freq[nums[i]] == 0){
+                freq[nums[i]] += 1;
+            } else {
+                return nums[i];
+            }
         }
-        return i;
+        return 0;
     }
 };
