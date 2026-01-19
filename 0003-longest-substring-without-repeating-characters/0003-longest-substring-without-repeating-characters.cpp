044ad1e97c50874665ca9e45675bd77f60f4b51e
AmanDeep <162536127+imaman2001@users.noreply.github.com>
2026-01-19 11:43:14 +0530

    Time: 3 ms (79.34%), Space: 11.5 MB (85.3%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -15,20 +15,40 @@ public:
     //     return maxl;
     // }
 
-    int lengthOfLongestSubstring(string s) {
+    // int lengthOfLongestSubstring(string s) {
+    //     int left = 0;
+    //     int n = s.length();
+    //     int maxLen = 0;
+    //     unordered_set<char> charSet;
+
+    //     for(int i=0; i<n; i++){
+    //         while(charSet.find(s[i]) != charSet.end()){
+    //             charSet.erase(s[left]);
+    //             left++;
+    //         }
+
+    //         charSet.insert(s[i]);
+    //         maxLen = max(maxLen , i - left + 1);
+    //     }
+    //     return maxLen;
+    // }
+
+// optimal solution
+    int lengthOfLongestSubstring(string s){
+        vector<int> mpp(256, -1);
         int left = 0;
+        int right = 0;
         int n = s.length();
         int maxLen = 0;
-        unordered_set<char> charSet;
 
-        for(int i=0; i<n; i++){
-            while(charSet.find(s[i]) != charSet.end()){
-                charSet.erase(s[left]);
-                left++;
+        while(right < n){
+            if(mpp[s[right]] != -1){
+                left = max(left , mpp[s[right]] + 1);
             }
 
-            charSet.insert(s[i]);
-            maxLen = max(maxLen , i - left + 1);
+            mpp[s[right]] = right;
+            maxLen = max(maxLen , right-left+1);
+            right++;
         }
         return maxLen;
     }
