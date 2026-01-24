afff2e44ba8f6f0457de4a51f0b8a6aae2f8c57b
AmanDeep <162536127+imaman2001@users.noreply.github.com>
2026-01-24 09:02:39 +0530

    Time: 0 ms (100%), Space: 16.5 MB (30.32%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -10,29 +10,53 @@
  */
 class Solution {
 public:
-    ListNode* rotateRight(ListNode* head, int k) {
-        if(!head) return head;
+    // ListNode* rotateRight(ListNode* head, int k) {
+    //     if(!head) return head;
 
-        int len = 1;
-        ListNode* dummy = head;
+    //     int len = 1;
+    //     ListNode* dummy = head;
 
-        while(dummy->next != NULL){
-            dummy = dummy->next;
-            len++;                //3
+    //     while(dummy->next != NULL){
+    //         dummy = dummy->next;
+    //         len++;                //3
+    //     }
+
+    //     int pos = k % len;  // 1
+    //     if(pos == 0) return head;   // no need to rotate
+
+    //     ListNode* curr = head;
+    //     for(int i=0; i < len - pos - 1; i++){
+    //         curr = curr->next;
+    //     }
+
+    //     ListNode* newHead = curr->next;
+    //     curr->next = nullptr;
+    //     dummy->next = head;
+
+    //     return newHead;
+    // }
+
+    ListNode* rotateRight(ListNode* head, int k){
+        if(head == NULL || head->next == NULL || k == 0){
+            return head;
         }
-
-        int pos = k % len;  // 1
-        if(pos == 0) return head;   // no need to rotate
-
-        ListNode* curr = head;
-        for(int i=0; i < len - pos - 1; i++){
-            curr = curr->next;
+        int count = 1;
+        ListNode* temp = head;
+        while(temp->next){
+            temp = temp->next;
+            count++;
         }
+        temp->next = head;
+        temp = head;
 
-        ListNode* newHead = curr->next;
-        curr->next = nullptr;
-        dummy->next = head;
+        int n = count - (k % count);
 
+        ListNode* prev = head;
+        for(int i=1; i<n; i++){
+            prev = prev->next;
+        }
+        ListNode* newHead = prev->next;
+        prev->next = NULL;
         return newHead;
     }
 };
