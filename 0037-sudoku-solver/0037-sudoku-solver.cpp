1160af6b89c57779297d6e072fef3990ae414f9a
AmanDeep <162536127+imaman2001@users.noreply.github.com>
2026-02-02 19:17:50 +0530

    Time: 499 ms (12.23%), Space: 8.9 MB (32.61%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,37 +1,45 @@
 class Solution {
     
 public:
+    // bool isSafe(vector<vector<char>>& board , int row , int col , char digit){
+    //     //check vertically
+    //     for(int j=0; j<9; j++){
+    //         if(board[row][j] == digit){
+    //             return false;
+    //         }
+    //     }
+        
+    //     //check horizontally
+    //     for(int i=0; i<9; i++){
+    //         if(board[i][col] == digit){
+    //             return false;
+    //         }
+    //     }
+        
+    //     //3X3 grid check
+    //     // int startRow = (row / 3)*3;
+    //     // int startCol = (col / 3)*3;
+    //     // for(int i=startRow; i<startRow+2; i++){
+    //     //     for(int j=startCol; j<startCol+2; j++){
+    //     //         if(board[i][j] == digit){
+    //     //             return false;
+    //     //         }
+    //     //     }
+    //     // }
+
+    //     for(int i=0; i<9; i++){
+    //         if(board[(row/3)*3 + i/3][(col/3)*3 + i%3] == digit){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
 
     bool isSafe(vector<vector<char>>& board , int row , int col , char digit){
-        //check vertically
-        for(int j=0; j<9; j++){
-            if(board[row][j] == digit){
-                return false;
-            }
-        }
-        
-        //check horizontally
         for(int i=0; i<9; i++){
-            if(board[i][col] == digit){
-                return false;
-            }
-        }
-        
-        //3X3 grid check
-        // int startRow = (row / 3)*3;
-        // int startCol = (col / 3)*3;
-        // for(int i=startRow; i<startRow+2; i++){
-        //     for(int j=startCol; j<startCol+2; j++){
-        //         if(board[i][j] == digit){
-        //             return false;
-        //         }
-        //     }
-        // }
-
-        for(int i=0; i<9; i++){
-            if(board[(row/3)*3 + i/3][(col/3)*3 + i%3] == digit){
-                return false;
-            }
+            if(board[i][col] == digit) return false;
+            if(board[row][i] == digit) return false;
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3] == digit) return false;
         }
         return true;
     }
