d6602e6e34d0ebe62ad9c50bd2ceb323ceafc705
AmanDeep <162536127+imaman2001@users.noreply.github.com>
2026-02-13 18:30:03 +0530

    Time: 15 ms (89.08%), Space: 139.2 MB (67.44%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,19 +1,39 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        priority_queue<pair<int , int>>pq;
-        vector<int> ans;
-        for(int i=0; i<k; i++){
-            pq.push(make_pair(nums[i], i));
-        }
-        ans.push_back(pq.top().first);
+        // priority_queue<pair<int , int>>pq;
+        // vector<int> ans;
+        // for(int i=0; i<k; i++){
+        //     pq.push(make_pair(nums[i], i));
+        // }
+        // ans.push_back(pq.top().first);
 
-        for(int i=k; i<nums.size(); i++){
-            while(!pq.empty() && pq.top().second <= (i-k)){
-                pq.pop();
+        // for(int i=k; i<nums.size(); i++){
+        //     while(!pq.empty() && pq.top().second <= (i-k)){
+        //         pq.pop();
+        //     }
+        //     pq.push(make_pair(nums[i], i));
+        //     ans.push_back(pq.top().first);
+        // }
+        // return ans;
+
+// using dequee
+        deque<int>dq;
+        vector<int>ans;
+        for(int i=0; i<nums.size(); i++){
+            if(!dq.empty() && dq.front() <= i-k){
+                dq.pop_front();
             }
-            pq.push(make_pair(nums[i], i));
-            ans.push_back(pq.top().first);
+
+            while(!dq.empty() && nums[dq.back()] < nums[i]){
+                dq.pop_back();
+            }
+
+            dq.push_back(i);
+
+            if(i >= k-1){
+                ans.push_back(nums[dq.front()]);
+            }
         }
         return ans;
     }
