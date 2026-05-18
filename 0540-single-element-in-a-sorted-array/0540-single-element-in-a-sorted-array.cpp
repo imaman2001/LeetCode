c009a6ca9e07dfc47994d861b4f86a39ded515ef
AmanDeep <162536127+imaman2001@users.noreply.github.com>
2026-05-18 07:27:08 +0530

    Time: 0 ms (100%), Space: 26.2 MB (33.36%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -15,7 +15,8 @@ public:
             if(nums[mid] != nums[mid+1] && nums[mid] != nums[mid-1]) return nums[mid];
 
             // this condition lies is the right half
-            if((mid%2 == 0) && nums[mid] == nums[mid+1] || (mid%2 != 0) && nums[mid] == nums[mid-1]){
+            if((mid%2 == 0) && nums[mid] == nums[mid+1] 
+            || (mid%2 != 0) && nums[mid] == nums[mid-1]){
                 low = mid+1;
             } else{
                 high = mid-1;
