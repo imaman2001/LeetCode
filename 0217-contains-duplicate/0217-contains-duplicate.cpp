615087eab8b0f86850980b275a5763bc3a6576be
AmanDeep <162536127+imaman2001@users.noreply.github.com>
2026-06-07 08:19:52 +0530

    Time: 71 ms (14.58%), Space: 93.1 MB (8.13%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums){
-        unordered_set<int> st;
-        for(int ele : nums){
-            st.insert(ele);
+        // unordered_set<int> st;
+        // for(int ele : nums){
+        //     st.insert(ele);
+        // }
+        // // remove duplicate elements from set it stores unique only 
+        // return st.size() < nums.size();
+
+        unordered_set<int>st;
+
+        for(int num : nums){
+            st.insert(num);
         }
-        // remove duplicate elements from set it stores unique only 
-        return st.size() < nums.size();
+        if(st.size() < nums.size()){
+            return true;
+        }
+
+        return false;
     }
 };
